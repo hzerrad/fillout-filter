7545be4fc15ca9f00df3200d355ff3d867e8bd00
Houssem Eddine Zerrad <hzerrad96@gmail.com>
2024-03-05 20:50:48 +0100

    add not found handler

--- src/app.ts
+++ src/app.ts
@@ -9,3 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', router);
+
+// not found handler
+app.use((req, res) => {
+	res.status(404).json({ message: 'Not Found' });
+});
